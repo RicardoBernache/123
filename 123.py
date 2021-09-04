05cb4ad7f6288e4cd01b88d7e6144d30362cc5b1
RicardoBernache <rica_201199@hotmail.com>
2021-09-04 02:54:02 -0500

    le cambie todo alv porque me vale verga y si quiere algo vayase a la verga

--- 123.py
+++ 123.py
@@ -1,4 +1,11 @@
-print('Hello World')
-print('el vernardo me pela la verga ')
-print('El adan es puto')
-print('Claro que si el adan es joto')
+import turtle
+import cv2
+import numpy as np
+
+turtle.color('blue')
+turtle.pencolor('black')
+turtle.
+cv2.polylines()
+for a in range(10):
+    turtle.circle(200)
+    input()
